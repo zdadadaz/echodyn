a24822be5f89171d503a3e555c9f42268b9a6d9f
zdadadaz <zdadadaz5566@gmail.com>
2020-03-31 20:08:01 +1000

    modify test_ef

--- playground/test_ef.py
+++ playground/test_ef.py
@@ -12,6 +12,81 @@ import sklearn.metrics
 import matplotlib.pyplot as plt
 import math
 
+
+def run_epoch(model, dataloader, phase, optim, device, save_all=False, blocks=None):
+
+    criterion = torch.nn.MSELoss()  # Standard L2 loss
+
+    runningloss = 0.0
+
+    model.train(phase == 'train')
+
+    counter = 0
+    summer = 0
+    summer_squared = 0
+
+    yhat = []
+    y = []
+
+    with torch.set_grad_enabled(phase == 'train'):
+        with tqdm.tqdm(total=len(dataloader)) as pbar:
+            for (i, (X, outcome)) in enumerate(dataloader):
+                
+                if i % 40==0:
+                    for i in range(torch.cuda.device_count()):
+                        torch.cuda.reset_max_memory_allocated(i)
+                        torch.cuda.reset_max_memory_cached(i)
+
+                y.append(outcome.numpy())
+                X = X.to(device)
+                outcome = outcome.to(device)
+
+                average = (len(X.shape) == 6)
+                if average:
+                    batch, n_crops, c, f, h, w = X.shape
+                    X = X.view(-1, c, f, h, w)
+
+                summer += outcome.sum()
+                summer_squared += (outcome ** 2).sum()
+
+                if blocks is None:
+                    outputs = model(X)
+                else:
+                    outputs = torch.cat([model(X[j:(j + blocks), ...]) for j in range(0, X.shape[0], blocks)])
+                
+                if save_all:
+                    yhat.append(outputs.view(-1).to("cpu").detach().numpy())
+                
+                if average:
+                    outputs = outputs.view(batch, n_crops, -1).mean(1)
+
+                if not save_all:
+                    yhat.append(outputs.view(-1).to("cpu").detach().numpy())
+
+                loss = criterion(outputs.view(-1), outcome)
+
+                if phase == 'train':
+                    optim.zero_grad()
+                    loss.backward()
+                    optim.step()
+
+                runningloss += loss.item() * X.size(0)
+                counter += X.size(0)
+
+                epoch_loss = runningloss / counter
+
+                # str(i, runningloss, epoch_loss,  str(((summer_squared) / counter - (summer / counter)**2).item()))
+
+                pbar.set_postfix_str("{:.2f} ({:.2f}) / {:.2f}".format(epoch_loss, loss.item(), summer_squared / counter - (summer / counter) ** 2))
+                pbar.update()
+
+    if not save_all:
+        yhat = np.concatenate(yhat)
+    y = np.concatenate(y)
+
+    return epoch_loss, yhat, y
+
+
 torch.cuda.empty_cache() 
 
 num_epochs=45
@@ -30,7 +105,7 @@ lr_step_period=None
 run_test=False
 run_extra_tests=False
 
-echonet.config.DATA_DIR = '../../db/EchoNet-Dynamic'
+echonet.config.DATA_DIR = '../../../data/EchoNet-Dynamic'
 
 ### Seed RNGs ###
 np.random.seed(seed)
@@ -65,6 +140,7 @@ kwargs = {"target_type": tasks,
           "period": period,
           }
 
+# load model
 checkpoint = torch.load(os.path.join(output, "best.pt"))
 model.load_state_dict(checkpoint['state_dict'])
 optim.load_state_dict(checkpoint['opt_dict'])
@@ -75,7 +151,7 @@ for split in ["val", "test"]:
     ds = echonet.datasets.Echo(split=split, **kwargs, crops="all")
     dataloader = torch.utils.data.DataLoader(
         ds, batch_size=1, num_workers=num_workers, shuffle=False, pin_memory=(device.type == "cuda"))
-    loss, yhat, y = echonet.utils.video.run_epoch(model, dataloader, split, None, device, save_all=True, blocks=100)
+    loss, yhat, y = run_epoch(model, dataloader, split, None, device, save_all=True, blocks=1)
 
     with open(os.path.join(output, "{}_predictions.csv".format(split)), "w") as g:
         for (filename, pred) in zip(ds.fnames, yhat):
@@ -116,70 +192,3 @@ for split in ["val", "test"]:
     plt.close(fig)
 
 
-def run_epoch(model, dataloader, phase, optim, device, save_all=False, blocks=None):
-
-    criterion = torch.nn.MSELoss()  # Standard L2 loss
-
-    runningloss = 0.0
-
-    model.train(phase == 'train')
-
-    counter = 0
-    summer = 0
-    summer_squared = 0
-
-    yhat = []
-    y = []
-
-    with torch.set_grad_enabled(phase == 'train'):
-        with tqdm.tqdm(total=len(dataloader)) as pbar:
-            for (i, (X, outcome)) in enumerate(dataloader):
-
-                y.append(outcome.numpy())
-                X = X.to(device)
-                outcome = outcome.to(device)
-
-                average = (len(X.shape) == 6)
-                if average:
-                    batch, n_crops, c, f, h, w = X.shape
-                    X = X.view(-1, c, f, h, w)
-
-                summer += outcome.sum()
-                summer_squared += (outcome ** 2).sum()
-
-                if blocks is None:
-                    outputs = model(X)
-                else:
-                    outputs = torch.cat([model(X[j:(j + blocks), ...]) for j in range(0, X.shape[0], blocks)])
-
-                if save_all:
-                    yhat.append(outputs.view(-1).to("cpu").detach().numpy())
-
-                if average:
-                    outputs = outputs.view(batch, n_crops, -1).mean(1)
-
-                if not save_all:
-                    yhat.append(outputs.view(-1).to("cpu").detach().numpy())
-
-                loss = criterion(outputs.view(-1), outcome)
-
-                if phase == 'train':
-                    optim.zero_grad()
-                    loss.backward()
-                    optim.step()
-
-                runningloss += loss.item() * X.size(0)
-                counter += X.size(0)
-
-                epoch_loss = runningloss / counter
-
-                # str(i, runningloss, epoch_loss,  str(((summer_squared) / counter - (summer / counter)**2).item()))
-
-                pbar.set_postfix_str("{:.2f} ({:.2f}) / {:.2f}".format(epoch_loss, loss.item(), summer_squared / counter - (summer / counter) ** 2))
-                pbar.update()
-
-    if not save_all:
-        yhat = np.concatenate(yhat)
-    y = np.concatenate(y)
-
-    return epoch_loss, yhat, y
