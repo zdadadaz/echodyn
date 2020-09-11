7e157268c005ad41ce0c49cd91cbe65e4b6417c0
JC <zdadadaz5566@gmail.com>
2020-09-11 00:50:54 +0000

    add r2plus1DRA model

---
+++ echonet/models/r2plus1DResidualAttention.py
@@ -0,0 +1,347 @@
+import torch
+import torch.nn as nn
+from torch.nn import init
+import functools
+from torch.autograd import Variable
+import numpy as np
+from torch.nn.modules.utils import _triple
+import math
+
+class SpatioTemporalConv(nn.Module):
+    r"""Applies a factored 3D convolution over an input signal composed of several input 
+    planes with distinct spatial and time axes, by performing a 2D convolution over the 
+    spatial axes to an intermediate subspace, followed by a 1D convolution over the time 
+    axis to produce the final output.
+    Args:
+        in_channels (int): Number of channels in the input tensor
+        out_channels (int): Number of channels produced by the convolution
+        kernel_size (int or tuple): Size of the convolving kernel
+        stride (int or tuple, optional): Stride of the convolution. Default: 1
+        padding (int or tuple, optional): Zero-padding added to the sides of the input during their respective convolutions. Default: 0
+        bias (bool, optional): If ``True``, adds a learnable bias to the output. Default: ``True``
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, bias=True):
+        super(SpatioTemporalConv, self).__init__()
+
+        # if ints are entered, convert them to iterables, 1 -> [1, 1, 1]
+        kernel_size = _triple(kernel_size)
+        stride = _triple(stride)
+        padding = _triple(padding)
+
+        # decomposing the parameters into spatial and temporal components by
+        # masking out the values with the defaults on the axis that
+        # won't be convolved over. This is necessary to avoid unintentional
+        # behavior such as padding being added twice
+        spatial_kernel_size =  [1, kernel_size[1], kernel_size[2]]
+        spatial_stride =  [1, stride[1], stride[2]]
+        spatial_padding =  [0, padding[1], padding[2]]
+
+        temporal_kernel_size = [kernel_size[0], 1, 1]
+        temporal_stride =  [stride[0], 1, 1]
+        temporal_padding =  [padding[0], 0, 0]
+
+        # compute the number of intermediary channels (M) using formula 
+        # from the paper section 3.5
+        intermed_channels = int(math.floor((kernel_size[0] * kernel_size[1] * kernel_size[2] * in_channels * out_channels)/ \
+                            (kernel_size[1]* kernel_size[2] * in_channels + kernel_size[0] * out_channels)))
+
+        # the spatial conv is effectively a 2D conv due to the 
+        # spatial_kernel_size, followed by batch_norm and ReLU
+        self.spatial_conv = nn.Conv3d(in_channels, intermed_channels, spatial_kernel_size,
+                                    stride=spatial_stride, padding=spatial_padding, bias=bias)
+        self.bn = nn.BatchNorm3d(intermed_channels)
+        self.relu = nn.ReLU()
+
+        # the temporal conv is effectively a 1D conv, but has batch norm 
+        # and ReLU added inside the model constructor, not here. This is an 
+        # intentional design choice, to allow this module to externally act 
+        # identical to a standard Conv3D, so it can be reused easily in any 
+        # other codebase
+        self.temporal_conv = nn.Conv3d(intermed_channels, out_channels, temporal_kernel_size, 
+                                    stride=temporal_stride, padding=temporal_padding, bias=bias)
+
+    def forward(self, x):
+        x = self.relu(self.bn(self.spatial_conv(x)))
+        x = self.temporal_conv(x)
+        return x
+
+class ResidualBlock(nn.Module):
+    def __init__(self, input_channels, output_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.stride = stride
+        self.bn1 = nn.BatchNorm3d(input_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv1 = SpatioTemporalConv(input_channels, output_channels//4, 1, 1, bias = False)
+        self.bn2 = nn.BatchNorm3d(output_channels//4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = SpatioTemporalConv(output_channels//4, output_channels//4, 3, stride, padding = 1, bias = False)
+        self.bn3 = nn.BatchNorm3d(output_channels//4)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv3 = SpatioTemporalConv(output_channels//4, output_channels, 1, 1, bias = False)
+        self.conv4 = SpatioTemporalConv(input_channels, output_channels , 1, stride, bias = False)
+        
+    def forward(self, x):
+        residual = x
+        out = self.bn1(x)
+        out1 = self.relu(out)
+        out = self.conv1(out1)
+        out = self.bn2(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn3(out)
+        out = self.relu(out)
+        out = self.conv3(out)
+        if (self.input_channels != self.output_channels) or (self.stride !=1 ):
+            residual = self.conv4(out1)
+        out += residual
+        return out
+
+class AttentionModule_stage1(nn.Module):
+    # input size is 32*56*56
+    def __init__(self, in_channels, out_channels, size1=(32, 56, 56), size2=(16, 28, 28), size3=(8, 14, 14)):
+        super(AttentionModule_stage1, self).__init__()
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax1_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.skip1_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool2 = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax2_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.skip2_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool3 = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax3_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+
+        self.interpolation3 = nn.Upsample(size=size3, mode = 'trilinear',align_corners=True)
+
+        self.softmax4_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.interpolation2 = nn.Upsample(size=size2, mode = 'trilinear',align_corners=True)
+
+        self.softmax5_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.interpolation1 = nn.Upsample(size=size1, mode = 'trilinear',align_corners=True)
+
+        self.softmax6_blocks = nn.Sequential(
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            SpatioTemporalConv(out_channels, out_channels , kernel_size = 1, stride = 1, bias = False),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            SpatioTemporalConv(out_channels, out_channels , kernel_size = 1, stride = 1, bias = False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.first_residual_blocks(x)
+        out_trunk = self.trunk_branches(x)
+        out_mpool1 = self.mpool1(x)
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+        out_skip1_connection = self.skip1_connection_residual_block(out_softmax1)
+        out_mpool2 = self.mpool2(out_softmax1)
+        out_softmax2 = self.softmax2_blocks(out_mpool2)
+        out_skip2_connection = self.skip2_connection_residual_block(out_softmax2)
+        out_mpool3 = self.mpool3(out_softmax2)
+        out_softmax3 = self.softmax3_blocks(out_mpool3)
+        
+        out_interp3 = self.interpolation3(out_softmax3) + out_softmax2
+        # print(out_skip2_connection.data)
+        # print(out_interp3.data)
+        out = out_interp3 + out_skip2_connection
+        out_softmax4 = self.softmax4_blocks(out)
+        out_interp2 = self.interpolation2(out_softmax4) + out_softmax1
+        out = out_interp2 + out_skip1_connection
+        out_softmax5 = self.softmax5_blocks(out)
+        out_interp1 = self.interpolation1(out_softmax5) + out_trunk
+        out_softmax6 = self.softmax6_blocks(out_interp1)
+        out = (1 + out_softmax6) * out_trunk
+        out_last = self.last_blocks(out)
+
+        return out_last
+
+class AttentionModule_stage2(nn.Module):
+    # input image size is 16*28*28
+    def __init__(self, in_channels, out_channels, size1=(16, 28, 28), size2=(8, 14, 14)):
+        super(AttentionModule_stage2, self).__init__()
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax1_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.skip1_connection_residual_block = ResidualBlock(in_channels, out_channels)
+
+        self.mpool2 = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+
+        self.softmax2_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+        self.interpolation2 = nn.Upsample(size=size2, mode = 'trilinear',align_corners=True)
+        
+        self.softmax3_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.interpolation1 = nn.Upsample(size=size1, mode = 'trilinear',align_corners=True)
+
+        self.softmax4_blocks = nn.Sequential(
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            SpatioTemporalConv(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            SpatioTemporalConv(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.first_residual_blocks(x)
+        out_trunk = self.trunk_branches(x)
+        out_mpool1 = self.mpool1(x)
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+        out_skip1_connection = self.skip1_connection_residual_block(out_softmax1)
+        out_mpool2 = self.mpool2(out_softmax1)
+        out_softmax2 = self.softmax2_blocks(out_mpool2)
+
+        out_interp2 = self.interpolation2(out_softmax2) + out_softmax1
+        # print(out_skip2_connection.data)
+        # print(out_interp3.data)
+        out = out_interp2 + out_skip1_connection
+        out_softmax3 = self.softmax3_blocks(out)
+        out_interp1 = self.interpolation1(out_softmax3) + out_trunk
+        out_softmax4 = self.softmax4_blocks(out_interp1)
+        out = (1 + out_softmax4) * out_trunk
+        out_last = self.last_blocks(out)
+
+        return out_last
+
+class AttentionModule_stage3(nn.Module):
+    # input image size is 8*14*14
+    def __init__(self, in_channels, out_channels, size1=(8, 14, 14)):
+        super(AttentionModule_stage3, self).__init__()
+        self.first_residual_blocks = ResidualBlock(in_channels, out_channels)
+
+        self.trunk_branches = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+         )
+
+        self.mpool1 = nn.MaxPool3d(kernel_size=3, stride=2, padding=1)
+        self.softmax1_blocks = nn.Sequential(
+            ResidualBlock(in_channels, out_channels),
+            ResidualBlock(in_channels, out_channels)
+        )
+
+        self.interpolation1 = nn.Upsample(size=size1, mode = 'trilinear',align_corners=True)
+        self.softmax2_blocks = nn.Sequential(
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            SpatioTemporalConv(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True),
+            SpatioTemporalConv(out_channels, out_channels, kernel_size=1, stride=1, bias=False),
+            nn.Sigmoid()
+        )
+
+        self.last_blocks = ResidualBlock(in_channels, out_channels)
+
+    def forward(self, x):
+        x = self.first_residual_blocks(x)
+        out_trunk = self.trunk_branches(x)
+        out_mpool1 = self.mpool1(x)
+        out_softmax1 = self.softmax1_blocks(out_mpool1)
+
+        out_interp1 = self.interpolation1(out_softmax1) + out_trunk
+        out_softmax2 = self.softmax2_blocks(out_interp1)
+        out = (1 + out_softmax2) * out_trunk
+        out_last = self.last_blocks(out)
+
+        return out_last
+
+class ResidualAttentionModel_92(nn.Module):
+    # for input size 112
+    def __init__(self):
+        super(ResidualAttentionModel_92, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv3d(3, 64, kernel_size=3, stride=1, padding=1, bias = False),
+            nn.BatchNorm3d(64),
+            nn.ReLU(inplace=True)
+        )
+        self.mpool1 = nn.MaxPool3d(kernel_size=3, stride=(1,2,2), padding=1)
+        self.residual_block1 = ResidualBlock(64, 256)
+        self.attention_module1 = AttentionModule_stage1(256, 256)
+        self.residual_block2 = ResidualBlock(256, 512, 2)
+        self.attention_module2 = AttentionModule_stage2(512, 512)
+        self.attention_module2_2 = AttentionModule_stage2(512, 512)  # tbq add
+        self.residual_block3 = ResidualBlock(512, 1024, 2)
+        self.attention_module3 = AttentionModule_stage3(1024, 1024)
+        self.attention_module3_2 = AttentionModule_stage3(1024, 1024)  # tbq add
+        self.attention_module3_3 = AttentionModule_stage3(1024, 1024)  # tbq add
+        self.residual_block4 = ResidualBlock(1024, 2048, 2)
+        self.residual_block5 = ResidualBlock(2048, 2048)
+        self.residual_block6 = ResidualBlock(2048, 2048)
+        self.mpool2 = nn.Sequential(
+            nn.BatchNorm3d(2048),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool3d(1)
+            # nn.AvgPool3d(kernel_size=(4,7,7), stride=1)
+        )
+        self.fc = nn.Linear(2048,1)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.mpool1(out)
+        # # print(out.data)
+        out = self.residual_block1(out)
+        out = self.attention_module1(out)
+        out = self.residual_block2(out)
+        out = self.attention_module2(out)
+        out = self.attention_module2_2(out)
+        out = self.residual_block3(out)
+        # print(out.data)
+        out = self.attention_module3(out)
+        out = self.attention_module3_2(out)
+        out = self.attention_module3_3(out)
+        out = self.residual_block4(out)
+        out = self.residual_block5(out)
+        out = self.residual_block6(out)
+        out = self.mpool2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
+
+# +
+# # model = ResidualBlock(64, 128)
+# model = ResidualAttentionModel_92()
+# model.cuda()
+# # X = torch.rand(1,64,32,112,112)
+# X = torch.rand(1,3,32,112,112).cuda()
+# # flow = torch.rand(3*2,2,32,112,112)
+# out = model(X)
+# print(out.shape)
